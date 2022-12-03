3e8460ed73703ac7350f95bfe314a8cefb7c2a45
jnar <jnar@debian.home>
2022-12-03 22:19:19 +0100

    provide executables with correct solution of day1

--- day1/day1_2.c
+++ day1/day1_2.c
@@ -3,6 +3,8 @@
 
 int main(){
 
+    int* pElfsCalArr = NULL;
+
     FILE* fP = fopen("../../day1/input", "r");
     if(fP == NULL){
         perror("Error opening file!");
@@ -15,7 +17,7 @@ int main(){
 
     int elfCalsMax = 0;
     int elfCals = 0;
-
+    int elfIndex = 0;
     while ((read = getline(&line, &len, fP)) != -1) {
 
         printf("Retrieved line of length %zu:\n", read);
@@ -31,12 +33,36 @@ int main(){
                 elfCalsMax = elfCals;
             }
 
+            int elfCount = elfIndex + 1;
+            pElfsCalArr = (int*)realloc(pElfsCalArr, elfCount * sizeof(int));
+            pElfsCalArr[elfIndex] = elfCals;
+
+            elfIndex++;
             elfCals = 0;
         }
     }
 
+
     fclose(fP);
+
+    int secondElfCals = 0;
+    for(int i = 0; i< elfIndex ; i++){
+        if(pElfsCalArr[i] > secondElfCals && pElfsCalArr[i] < elfCalsMax){
+            secondElfCals = pElfsCalArr[i];
+        }
+    }
+
+    int thirdElfCals = 0;
+    for(int i = 0; i< elfIndex ; i++){
+        if(pElfsCalArr[i] > thirdElfCals && pElfsCalArr[i] < secondElfCals){
+            thirdElfCals = pElfsCalArr[i];
+        }
+    }
+
+    free(pElfsCalArr);
+
     printf("Elf with most calories has %d", elfCalsMax);
-    
+    printf("Total calories top three %d", elfCalsMax + secondElfCals + thirdElfCals);
+
     return 0;
 }
