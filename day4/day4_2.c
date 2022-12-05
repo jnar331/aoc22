a9b1b20d00169631c54c1f967620b09800993688
jnar <jnar@debian.home>
2022-12-05 22:11:59 +0100

    provide executables with correct solutions for day4

---
+++ day4/day4_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Sections{
+    int start;
+    int end;
+};
+
+int main(){
+    FILE* fP = fopen("../../day4/input", "r");
+    if(fP == NULL){
+        perror("Error opening file!");
+        exit(-1);
+    }
+
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    struct Sections firstElf;
+    struct Sections secondElf;
+    int overlappingPairs = 0;
+    while ((read = getline(&line, &len, fP)) != -1) {
+
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+        sscanf(line, "%d-%d,%d-%d\n", &firstElf.start,&firstElf.end,&secondElf.start,&secondElf.end);
+        if((firstElf.start <= secondElf.end && firstElf.end >= secondElf.start) || (secondElf.start <= firstElf.end && secondElf.end >= firstElf.start)){
+            overlappingPairs++;
+        }
+    }
+
+    fclose(fP);
+    printf("Count of overlapping pairs: %d\n", overlappingPairs);
+
+    return 0;
+}
